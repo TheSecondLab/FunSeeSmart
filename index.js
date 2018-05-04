d102aec5d422d25263c6a20df2c4b2a3412e8a4e
xuqiuyu <xuqiuyu@zhongan.com>
2018-05-04 16:45:52 +0800

    :tada: funsee-smart first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import { smart, autobind } from './lib/util/decorators';
+import { fieldBuilder, formBuilder, tableBuilder } from './lib/util/generator';
+import { SearchForm, Table, Pagination, EditForm, CRUD } from './lib/components';
+
+export default {
+  smart,
+  autobind,
+  fieldBuilder,
+  formBuilder,
+  tableBuilder,
+  SearchForm,
+  Table,
+  Pagination,
+  EditForm,
+  CRUD
+};
+
